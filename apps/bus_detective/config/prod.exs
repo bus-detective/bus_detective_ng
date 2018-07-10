0a18028fe0ded695fe2742869abc49ba73a316fa
Tim Mecklem <timothy@mecklem.com>
2018-07-10 17:51:40 -0400

    Configure app for heroku

--- apps/bus_detective/config/prod.exs
+++ apps/bus_detective/config/prod.exs
@@ -1,3 +1,8 @@
 use Mix.Config
 
-import_config "prod.secret.exs"
+config :bus_detective, BusDetective.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true,
+  types: BusDetective.PostgresTypes
