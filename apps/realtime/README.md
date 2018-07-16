cc65ed757f08ffd0fa3eec804a403f3dc0a7ace4
Tim Mecklem <timothy@mecklem.com>
2018-07-16 07:21:47 -0400

    `mix new realtime --sup`

---
+++ apps/realtime/README.md
@@ -0,0 +1,21 @@
+# Realtime
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `realtime` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:realtime, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/realtime](https://hexdocs.pm/realtime).
+
---
+++ apps/realtime/mix.exs
@@ -0,0 +1,48 @@
+defmodule Realtime.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      aliases: aliases(Mix.env()),
+      app: :realtime,
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps: deps(),
+      deps_path: "../../deps",
+      elixir: "~> 1.6",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      lockfile: "../../mix.lock",
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.travis": :test,
+        "coveralls.html": :test
+      ],
+      start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      version: "0.1.0"
+    ]
+  end
+
+  defp aliases(:dev), do: []
+
+  defp aliases(_) do
+    [compile: "compile --warnings-as-errors"]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Realtime.Application, []}
+    ]
+  end
+
+  defp deps do
+    []
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+end
