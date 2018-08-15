f8434cbfb16c1754ef280979ddaa773b6924d765
Tim Mecklem <timothy@mecklem.com>
2018-08-15 08:01:45 -0400

    Add class for past departures

--- apps/bus_detective_web/assets/js/departure.js
+++ apps/bus_detective_web/assets/js/departure.js
@@ -25,7 +25,7 @@ class Departure extends HTMLElement {
 
     template.innerHTML = `
       <link rel="stylesheet" href="/css/app.css">
-      <div class="timeline__event event event--future" data-test="departure_item">
+      <div class="timeline__event event event--future" id="departure-item">
         <div class="event__time">${this.formattedDepartureTime}</div>
         <div class="event__event-details">
           <div class="event__marker"></div>
@@ -43,6 +43,25 @@ class Departure extends HTMLElement {
 
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback () {
+    const update = () => {
+      let rootDiv = this.shadowRoot.querySelector('#departure-item');
+      if (this.isPast()) {
+        rootDiv.classList.add('event--past');
+        rootDiv.classList.remove('event--future');
+      } else {
+        rootDiv.classList.add('event--future');
+        rootDiv.classList.remove('event--past');
+      }
+      setTimeout(update, 5000);
+    };
+    update();
+  }
+
+  isPast () {
+    return new Date(this.departureTime) < new Date();
   }
 }
 
