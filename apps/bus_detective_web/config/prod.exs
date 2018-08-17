3486f6faf66a3861a3fe282ab6f25c0d313abaeb
Tim Mecklem <timothy@mecklem.com>
2018-08-17 18:13:06 -0400

    Use env variable for production host

--- apps/bus_detective_web/config/prod.exs
+++ apps/bus_detective_web/config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :bus_detective_web, BusDetectiveWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "busdetective-ng-staging.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("APP_DOMAIN_NAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
