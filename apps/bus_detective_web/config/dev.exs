bec2b9b01d90c0799d9dacbc3d8b56553d786c2b
Tim Mecklem <timothy@mecklem.com>
2018-08-27 08:08:26 -0400

    Add comment for how to bind to any address

--- apps/bus_detective_web/config/dev.exs
+++ apps/bus_detective_web/config/dev.exs
@@ -8,6 +8,7 @@ use Mix.Config
 # with brunch.io to recompile .js and .css sources.
 config :bus_detective_web, BusDetectiveWeb.Endpoint,
   http: [port: 4000],
+  # http: [ip: {0, 0, 0, 0}, port: 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
